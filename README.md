07d54d3baefda4b46bde5bcc53c167723ba072f1
vincephung <vincentphung43@yahoo.com>
2020-09-12 21:39:23 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Project for the odin project
-
-React app that uses the OpenWeatherApp API to display the weater for a certain location
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FormContainer from './FormContainer';
-
-document.body.style.background = '#c5e2f7';
-
-const App = () => <FormContainer />;
-
-export default App;
