b6822ac4487b6e584640620b92a48f2ca31ff0cd
vincephung <vincentphung43@yahoo.com>
2020-09-12 22:31:09 -0400

    Updates

---
+++ precache-manifest.19bd41829c090d9f583323ef16615a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d20de4361efb8d2e583d1f86bd7c34",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "0378d3898b507e5f242d",
+    "url": "/weather-app/static/css/main.dcb5aade.chunk.css"
+  },
+  {
+    "revision": "bc78efbc497b9d578762",
+    "url": "/weather-app/static/js/2.a09743d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.a09743d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0378d3898b507e5f242d",
+    "url": "/weather-app/static/js/main.3ede868f.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
